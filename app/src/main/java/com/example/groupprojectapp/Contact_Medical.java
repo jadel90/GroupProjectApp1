ba8e5c8a72cf07a94dec83207ffbbc8b9ddcf6d4
jadel90 <146587728+jadel90@users.noreply.github.com>
2023-11-09 23:04:04 +0000

    Update Contact_Medical.java

--- app/src/main/java/com/example/groupprojectapp/Contact_Medical.java
+++ app/src/main/java/com/example/groupprojectapp/Contact_Medical.java
@@ -25,7 +25,7 @@ public class Contact_Medical extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, MainActivity3.class);
+                Intent intent = new Intent(context, MainActivity4.class);
                 context.startActivity(intent);
                 Toast.makeText(context, "FAB Clicked", Toast.LENGTH_LONG).show();
             }
