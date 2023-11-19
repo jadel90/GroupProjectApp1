31facfcf8d902c0e609b53f987a80151134c895b
jadel90 <146587728+jadel90@users.noreply.github.com>
2023-11-19 13:18:50 +0000

    Update Contact_Insurance.java

--- app/src/main/java/com/example/groupprojectapp/Contact_Insurance.java
+++ app/src/main/java/com/example/groupprojectapp/Contact_Insurance.java
@@ -6,19 +6,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class Contact_Insurance extends AppCompatActivity {
 
-    // Firebase database reference
-    private DatabaseReference mDatabase;
+    // Firebase Firestore reference
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference insuranceInquiriesRef = db.collection("insurance_inquiries");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +42,6 @@ public class Contact_Insurance extends AppCompatActivity {
                 Toast.makeText(Contact_Insurance.this, "FAB Clicked", Toast.LENGTH_LONG).show();
             }
         });
-
-        // Initialize Firebase Database
-        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         // Initialize form fields
         EditText nameEditText = findViewById(R.id.editTextText11);
@@ -61,26 +63,26 @@ public class Contact_Insurance extends AppCompatActivity {
                 if (name.isEmpty() || company.isEmpty() || phone.isEmpty() || message.isEmpty()) {
                     Toast.makeText(Contact_Insurance.this, "Please fill out all fields.", Toast.LENGTH_LONG).show();
                 } else {
-                    // Create a HashMap with user's data
-                    HashMap<String, Object> insuranceData = new HashMap<>();
+                    // Create a Map with user's data
+                    Map<String, Object> insuranceData = new HashMap<>();
                     insuranceData.put("name", name);
                     insuranceData.put("company", company);
                     insuranceData.put("phone", phone);
                     insuranceData.put("message", message);
 
-                    // Save the inquiry data to Firebase
-                    saveInquiryToFirebase(insuranceData);
+                    // Save the inquiry data to Firestore
+                    saveInquiryToFirestore(insuranceData);
                 }
             }
         });
     }
 
-    private void saveInquiryToFirebase(HashMap<String, Object> insuranceData) {
-        // Push creates a unique id for each new entry
-        mDatabase.child("insurance_inquiries").push().setValue(insuranceData)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
+    private void saveInquiryToFirestore(Map<String, Object> insuranceData) {
+        // Add the data to Firestore
+        insuranceInquiriesRef.add(insuranceData)
+                .addOnCompleteListener(new OnCompleteListener() {
                     @Override
-                    public void onComplete(@NonNull Task<Void> task) {
+                    public void onComplete(@NonNull Task task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(Contact_Insurance.this, "Inquiry submitted successfully.", Toast.LENGTH_LONG).show();
                             // Optionally clear the fields after submission
@@ -94,9 +96,14 @@ public class Contact_Insurance extends AppCompatActivity {
 
     private void clearFormFields() {
         // Clear the input fields after successful form submission
-        ((EditText)findViewById(R.id.editTextText11)).setText("");
-        ((EditText)findViewById(R.id.editTextText14)).setText("");
-        ((EditText)findViewById(R.id.editTextPhone)).setText("");
-        ((EditText)findViewById(R.id.editTextTextMultiLine)).setText("");
+        ((EditText) findViewById(R.id.editTextText11)).setText("");
+        ((EditText) findViewById(R.id.editTextText14)).setText("");
+        ((EditText) findViewById(R.id.editTextPhone)).setText("");
+        ((EditText) findViewById(R.id.editTextTextMultiLine)).setText("");
     }
 }
+
+
+
+
+
