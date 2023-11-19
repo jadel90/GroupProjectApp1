94b7375f2c7c354d28df47775c510e52394d3bc5
jadel90 <146587728+jadel90@users.noreply.github.com>
2023-11-19 13:42:13 +0000

    Update rating.java

--- app/src/main/java/com/example/groupprojectapp/rating.java
+++ app/src/main/java/com/example/groupprojectapp/rating.java
@@ -9,58 +9,47 @@ import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class rating extends AppCompatActivity {
 
-
     Context context;
     FloatingActionButton fab;
     RatingBar ratingBar;
     EditText feedbackText;
     Button sendFeedbackButton;
-    DatabaseReference mDatabase;
 
+    // Firebase Firestore reference
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference ratingsRef = db.collection("ratings");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
 
-
-        fab = findViewById(R.id.fab); // Initialize the fab button
+        fab = findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(rating.this, MainActivity4.class);
-                context.startActivity(intent);
-                Toast.makeText(context, "FAB Clicked", Toast.LENGTH_LONG).show();
+                startActivity(intent);
+                Toast.makeText(rating.this, "FAB Clicked", Toast.LENGTH_LONG).show();
             }
         });
 
-
-        // Initialize Firebase Database
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-
-        // Initialize the UI components
         ratingBar = findViewById(R.id.ratingBar);
         feedbackText = findViewById(R.id.editTextText3);
         sendFeedbackButton = findViewById(R.id.button3);
-        fab = findViewById(R.id.fab);
 
-
-        // Setup the Send Feedback button click listener
         sendFeedbackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,43 +61,32 @@ public class rating extends AppCompatActivity {
                     return;
                 }
 
-                // Validate feedback text
                 if (feedback.isEmpty()) {
                     Toast.makeText(rating.this, "Please provide some feedback.", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                // Save the feedback to Firebase Database
-                saveFeedbackToFirebase(ratingValue, feedback);
+                // Save the feedback to Firestore
+                saveFeedbackToFirestore(ratingValue, feedback);
             }
         });
     }
 
-    private void saveFeedbackToFirebase(float rating, String feedback) {
-        // Create a unique ID for each feedback
-        String feedbackId = mDatabase.push().getKey();
-
-        // Create a HashMap with feedback data
+    private void saveFeedbackToFirestore(float rating, String feedback) {
+        // Create a Map with feedback data
         Map<String, Object> feedbackData = new HashMap<>();
         feedbackData.put("rating", rating);
         feedbackData.put("feedback", feedback);
 
-        // Save the feedback to the "ratings" node in Firebase Database
-        mDatabase.child("ratings").child(feedbackId).setValue(feedbackData)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(rating.this, "Thank you for your feedback!", Toast.LENGTH_LONG).show();
-                            ratingBar.setRating(0);
-                            feedbackText.setText("");
-                        } else {
-                            Toast.makeText(rating.this, "Failed to send feedback. Please try again.", Toast.LENGTH_LONG).show();
-                        }
-                    }
+        // Add the data to Firestore
+        ratingsRef.add(feedbackData)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(rating.this, "Thank you for your feedback!", Toast.LENGTH_LONG).show();
+                    ratingBar.setRating(0);
+                    feedbackText.setText("");
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(rating.this, "Failed to send feedback. Please try again.", Toast.LENGTH_LONG).show();
                 });
     }
 }
-
-
-
